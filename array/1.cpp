7be2ef13167dabcdb6ec8bba73526b95848f84a6
Omkar_Khar7 <okharwade.ok@gmail.com>
2026-05-30 13:26:38 +0530

    Add files via upload

---
+++ array/1.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[1]={2};
+    cout<<0[arr];
+}
---
+++ array/9.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i=0;
+    char[4]={'\0','\0','\0','\0'};
+    for(int i=0;i<4;i++){
+        cout<<s[i];
+    }
+
+    return 0;
+}
+// No output 
+// \0 =NULL thus compiler prints nothing 
